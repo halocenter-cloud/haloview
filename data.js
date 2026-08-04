4d80b2585e0168f080d040646620221210a83078
halocenter-cloud <softcenterlua@gmail.com>
2026-08-03 22:00:46 -0500

    chore: sync ranking from HaloBackend

--- data.js
+++ data.js
@@ -3,43 +3,43 @@
  */
 const SQUAD_DATA = {
   "season": "Temporada 9 — desde 3 ago 2026",
-  "lastUpdated": "2026-08-04T02:51:52.965Z",
+  "lastUpdated": "2026-08-04T03:00:46.041Z",
   "players": [
     {
       "id": 1,
       "name": "tagoras5268",
       "rank": 1,
-      "points": 68
+      "points": 73
     },
     {
       "id": 2,
       "name": "Andrei2589",
       "rank": 2,
-      "points": 65
+      "points": 70
     },
     {
       "id": 3,
       "name": "Andrey2k94",
       "rank": 3,
-      "points": 45
+      "points": 46
     },
     {
       "id": 4,
       "name": "Kairos5404",
       "rank": 4,
-      "points": 35
+      "points": 36
     },
     {
       "id": 5,
       "name": "ArgonautaElite1",
       "rank": 5,
-      "points": 30
+      "points": 35
     },
     {
       "id": 6,
       "name": "ESPECTRO5118",
       "rank": 6,
-      "points": 27
+      "points": 28
     },
     {
       "id": 7,
