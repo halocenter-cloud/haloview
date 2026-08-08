8ae878ef167c7708c24d52e33cee287ec363783e
halocenter-cloud <softcenterlua@gmail.com>
2026-08-07 23:43:16 -0500

    chore: sync ranking from HaloBackend

--- data.js
+++ data.js
@@ -3,108 +3,108 @@
  */
 const SQUAD_DATA = {
   "season": "Temporada 1 — desde 8 ago 2026",
-  "lastUpdated": "2026-08-08T00:34:38.604Z",
+  "lastUpdated": "2026-08-08T04:43:15.643Z",
   "players": [
     {
       "id": 1,
-      "name": "Andrei2589",
+      "name": "Andrey2k94",
       "rank": 1,
-      "points": 0
+      "points": 10
     },
     {
       "id": 2,
-      "name": "Andrey2k94",
-      "rank": 1,
-      "points": 0
+      "name": "Gargolaa6235",
+      "rank": 2,
+      "points": 8
     },
     {
       "id": 3,
-      "name": "Anzelot 5737",
-      "rank": 1,
-      "points": 0
+      "name": "Kairos5404",
+      "rank": 3,
+      "points": 6
     },
     {
       "id": 4,
-      "name": "ArgonautaElite1",
-      "rank": 1,
-      "points": 0
+      "name": "tagoras5268",
+      "rank": 4,
+      "points": 4
     },
     {
       "id": 5,
-      "name": "AsturiA4395",
-      "rank": 1,
-      "points": 0
+      "name": "Palitha96xD",
+      "rank": 5,
+      "points": 2
     },
     {
       "id": 6,
-      "name": "ESPECTRO5118",
-      "rank": 1,
-      "points": 0
+      "name": "ArgonautaElite1",
+      "rank": 6,
+      "points": 1
     },
     {
       "id": 7,
-      "name": "Gargolaa6235",
-      "rank": 1,
-      "points": 0
+      "name": "ESPECTRO5118",
+      "rank": 6,
+      "points": 1
     },
     {
       "id": 8,
-      "name": "JOSEMAGUZMO4346",
-      "rank": 1,
+      "name": "Andrei2589",
+      "rank": 7,
       "points": 0
     },
     {
       "id": 9,
-      "name": "Kairos5404",
-      "rank": 1,
+      "name": "Anzelot 5737",
+      "rank": 7,
       "points": 0
     },
     {
       "id": 10,
-      "name": "KHKgolber",
-      "rank": 1,
+      "name": "AsturiA4395",
+      "rank": 7,
       "points": 0
     },
     {
       "id": 11,
-      "name": "Mariana",
-      "rank": 1,
+      "name": "JOSEMAGUZMO4346",
+      "rank": 7,
       "points": 0
     },
     {
       "id": 12,
-      "name": "Palitha96xD",
-      "rank": 1,
+      "name": "KHKgolber",
+      "rank": 7,
       "points": 0
     },
     {
       "id": 13,
-      "name": "tagoras5268",
-      "rank": 1,
+      "name": "Mariana",
+      "rank": 7,
       "points": 0
     },
     {
       "id": 14,
       "name": "TAILONG0108R",
-      "rank": 1,
+      "rank": 7,
       "points": 0
     },
     {
       "id": 15,
       "name": "Tony91653",
-      "rank": 1,
+      "rank": 7,
       "points": 0
     },
     {
       "id": 16,
       "name": "Vellega",
-      "rank": 1,
+      "rank": 7,
       "points": 0
     },
     {
       "id": 17,
       "name": "Wikzork26",
-      "rank": 1,
+      "rank": 7,
       "points": 0
     }
   ]
